9eaa1f7784c9d1cba073ac53b3195e45ae9f3a4f
Eder Salim Martinez <edermtz.04@gmail.com>
2012-05-14 12:07:50 -0500

    Constantes

--- grails-app/domain/saum/Ingrediente.groovy
+++ grails-app/domain/saum/Ingrediente.groovy
@@ -1,5 +1,5 @@
 package saum
-
+import org.saum.Constantes
 class Ingrediente {
 	BigDecimal cantidad
 	String unidadMedida
@@ -8,7 +8,7 @@ class Ingrediente {
 
     static constraints = {
 		cantidad scale:2, precision:8, blank:false
-		unidadMedida inList:[Constantes.UNIDAD_MEDIDA_KILOGRAMO,'gr','l','ml','pza']
+		unidadMedida inList:[Constantes.UNIDAD_MEDIDA_KILOGRAMO,Constantes.UNIDAD_MEDIDA_GRAMO,Constantes.UNIDAD_MEDIDA_LITRO,Constantes.UNIDAD_MEDIDA_MILILITRO,Constantes.UNIDAD_MEDIDA_PIEZA]
 		presentacion maxSize:40
 		etapa maxSize:40, default:'General'
     }
