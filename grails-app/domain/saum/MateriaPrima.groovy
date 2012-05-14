004a6a91279a6092d1f9ae95bb80e2395d008673
Aaron Meza <aronmeza@hotmail.com>
2012-05-14 11:01:36 -0500

    Materia Prima domain

---
+++ grails-app/domain/saum/MateriaPrima.groovy
@@ -0,0 +1,19 @@
+package saum
+
+class MateriaPrima {
+	String unidadMedida
+	String nombre
+    
+    static constraints = {
+		nombre unique:true, maxSize:128, blank:false
+		unidadMedida inList:['kg','gr','l','ml','pza'], blank:false
+    }
+
+    static namedQueries = {
+		buscaPorNombre{ filtro ->
+			filtro="${filtro.toUpperCase()}%"
+			ilike 'nombre', filtro
+			}
+	}
+
+}
