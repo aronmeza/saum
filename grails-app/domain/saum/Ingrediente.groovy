154c8927cdc53ee114630b4a591ca18e3284d089
Aaron Meza <aronmeza@hotmail.com>
2012-05-14 11:21:59 -0500

    Cambio de lista en constantes

--- grails-app/domain/saum/Ingrediente.groovy
+++ grails-app/domain/saum/Ingrediente.groovy
@@ -1,7 +1,20 @@
 package saum
 
 class Ingrediente {
+	BigDecimal cantidad
+	String unidadMedida
+	String presentacion
+	static belongsTo=[etapa:Etapa, materia:MateriaPrima]	
 
     static constraints = {
+		cantidad scale:2, precision:8, blank:false
+		unidadMedida inList:[Constantes.UNIDAD_MEDIDA_KILOGRAMO,'gr','l','ml','pza']
+		presentacion maxSize:40
+		etapa maxSize:40, default:'General'
+    }
+    
+    String toString(){
+        return cantidad + " " + unidadMedida + " " + materia.nombre +" " +presentacion + " " + etapa
+        
     }
 }
